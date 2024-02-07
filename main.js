54b9ed613be60b940af0d936b6bf55b1c610db7b
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-02-07 09:14:30 -0500

    Add functionality to main page. Reset grid working. Trying to get blue color working & error on too few or too many squares.

--- main.js
+++ main.js
@@ -11,9 +11,10 @@ const blueBtn = document.querySelector('#blueBtn');
 const randomBtn = document.querySelector('#randomBtn');
 const clearBtn = document.querySelector('#clearBtn');
 const inputHint = document.querySelector('#hint');
+const boardGrid = document.querySelector('#board');
 
-// Event listener for clicking the input field.
-inputSize.addEventListener('click', showInputHint);
+// Event listeners 
+inputSize.addEventListener('click', showInputHint); //clicking the input field
 
 // Setting board div size
 const board = document.querySelector('#board');
@@ -21,10 +22,13 @@ board.style.width = board.style.height = `${gridSize}px`;
 
 // Function to create the grid
 function createBoard() {
-    let boardSize = inputSize.value;
+    let boardSize = Number(inputSize.value);
     const numDivs = (boardSize * boardSize);
 
     for(let i = 0; i < numDivs; i++) {
+        if(boardSize < 2 || boardSize > 99) {
+            inputHint.textContent = "Error. Please input a number between 2 and 99.";
+        } else {
         const boardCell = document.createElement('div');
 
         boardCell.style.width = boardCell.style.height = `${(gridSize/boardSize) - 2}px`;
@@ -32,12 +36,18 @@ function createBoard() {
         boardCell.classList.add('cell');
         board.appendChild(boardCell);
             
+        boardCell.addEventListener('mouseover', changeColorBlk);
         }
 
+        // blueBtn.addEventListener('click', function (e) {
+        //     const boardCell = document.querySelector('.cell');
+        //     boardCell.addEventListener('mouseover', changeColorBlue);
+        // })
+    }
     }
 
 // Function to delete grids upon new size entry.
-function removeGridCells() {
+function removeCells() {
     while(board.firstChild) {
         board.removeChild(board.firstChild);
     }
@@ -48,9 +58,18 @@ function showInputHint() {
     inputHint.textContent = "Enter a value between 2 and 99.";
 }
 
+function removeInputHint() {
+    inputHint.textContent = "";
+}
+
 // Function to change color when hovered over.
-function changeColor() {
+function changeColorBlk() {
     this.style.backgroundColor = "black";
+
+}
+
+function changeColorBlue() {
+    this.style.backgroundColor = "blue";
 }
 
 // Runs function to create board.
